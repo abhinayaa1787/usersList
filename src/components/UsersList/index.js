5b0b0741bb6dd4575b152538c1f55a134516c88e
Abhinayaa <abhinayaa1787@gmail.com>
2021-03-25 15:57:33 -0400

    Added pseudocode 2nd time

--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import React from 'react';
 // import ReactDOM from 'reactDom';
 // import axios from 'axios';
 // import ReactDOM from 'react-dom';
+
 
 class UsersList extends React.Component{
     // Setting state for the component
@@ -10,7 +12,7 @@ class UsersList extends React.Component{
     }
 componentDidMount(){
     // Fetching users from an endpoint 
-    fetch('http://exercises.getsandbox.com/users')
+    axios.get('http://exercises.getsandbox.com/users')
         .then(res=>
            this.setState({users:res.data}))
         .catch(error => {
@@ -21,38 +23,15 @@ componentDidMount(){
 // Traversing the users list to fetch last name, first name
 render(){
     return(
+//If the users arra is empty then the page displas loading(as the axios.get is fetching data)
+        const list=   ({this.state.users.map(user=> 
+            <li key={user.id} style={{listStyleType:"none", marginLeft:"50px", lineHeight:"2em"}}>{user.lastName},{user.firstName}</li>)};
         <ul>
             <h2>List of users:</h2>
-            {this.state.users.map(user=> 
-                <li key={user.id} style={{listStyleType:"none", marginLeft:"50px", lineHeight:"2em"}}>{user.lastName},{user.firstName}</li>)}
+            {this.state.users.length ? {list}: "Loading..."}
         </ul>
     )
 }
 };
 
-
-// function UsersList(){
-//     var users=[];
-
-//         axios.get('http://exercises.getsandbox.com/users')
-//         .then(res=>
-//            ({users:res.data}))
-//         .catch(error => {
-//             console.log(error);
-//         })
-
-// console.log(users);
-// ReactDOM.render(
-//         <ul>
-//             <h2>List of users:</h2>
-//             {users.map(user=> 
-//                 <li key={user.id} style={{listStyleType:"none", marginLeft:"50px", lineHeight:"2em"}}>{user.lastName},{user.firstName}</li>)}
-//         </ul>, document.getElementById("root")
-//     );
-    
-
-//             }
 export default UsersList;
-
-
- 
